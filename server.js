5af4caa0147d48e2e8e6cd76493e28c9f1ecc874
wihyanghoon <gidgns1995@gmail.com>
2022-12-09 23:53:02 +0900

    글 수정 기능 추가

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express'); // 라이브러리를 참조해주세요.
+const methodOverride = require('method-override')
 const app = express();
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 const MongoClient = require('mongodb').MongoClient;
 
@@ -71,10 +73,38 @@ app.get('/detail/:id', function (req, res) {
     })
 })
 
+app.get('/edit/:id', function (req, res) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (error, result) {
+        if (!result) {
+            res.write("<script>alert('no, data')</script>");
+            res.write("<script>window.location=\"/list\"</script>");
+        }
+        if (result) {
+            console.log(result)
+            res.render('edit.ejs', { data: result })
+        }
+    })
+})
+
+app.put('/edit', function (req, res) {
+    // db.collection('counter12ㅇㄹㅇㄹㅇㄹㅇ').updateOne({ _id: parseInt(req.body.id) }, { $set: { 제목: req.body.title, 내용: req.body.date } }, function (err, result) {
+    //     if (result.ismaster.ok <= 0) {
+    //         return res.status(400).send({ message: '실패' })
+    //     }
+    //     console.log(result)
+    //     res.status(200).send({ message: '성공했습니다.' })
+    // })
+    db.collection('fdfd').find()
+})
+
 app.delete('/delete', function (req, res) {
     req.body._id = parseInt(req.body._id)
     db.collection('post').deleteOne(req.body, function (err, result) {
         console.log('삭제완료')
         res.status(200).send({ message: '성공했습니다.' });
     })
+})
+
+app.get('/test', function (req, res) {
+    db.collection('df').find()
 })
